0d9155bca2cbe41275b862fbe3813aeb14cca0c4
maveobi <maveobi@example.com>
2019-09-29 16:45:53 +0200

    First post.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='terminalmagic',
+      version='0.3',
+      description='Convenience functions for using the terminal in ways you didn\'t know were possible.',
+      url='https://github.com/hiuka/terminalmagic',
+      author='hiuka',
+      author_email='nospam@example.com',
+      license='MIT',
+      packages=['terminalmagic'],
+      zip_safe=False)
