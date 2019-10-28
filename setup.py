35dc33b3b4dc86fa0b050307f4c67a6edfc5ef99
maveobi <55681994+maveobi@users.noreply.github.com>
2019-10-28 14:58:23 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='terminalmagic',
-      version='0.4',
+      version='0.5',
       description='Convenience functions for using the terminal in ways you didn\'t know were possible.',
       url='https://github.com/maveobi/terminalmagic',
       author='maveobi',
